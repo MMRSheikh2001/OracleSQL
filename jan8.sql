8c10fab8babda5cf387fc856c35846ae9e5e45bc
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-08 16:03:00 +0600

    n

--- jan8.sql
+++ jan8.sql
@@ -1,2 +1,22 @@
-select *
-from hr.employees;
+select first_name , last_name , salary,
+JOB_ID  ,LOCATION_ID,DEPARTMENT_NAME 
+from hr.employees
+NATURAL join  departments;
+
+desc  employees;
+desc departments;
+desc jobs;
+
+select EMPLOYEE_ID, FIRST_NAME , LAST_NAME , JOB_ID ,JOB_TITLE ,MAX_SALARY
+from employees
+natural join jobs
+where salary > 8000
+order by salary
+;
+
+select employee_id , last_name ,
+    location_id , department_id
+    from employees join departments
+    using (department_id);
+    
+    
