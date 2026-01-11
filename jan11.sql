bd7fd08994ae2b9aa7b8f5c7fc57da1aa795c731
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-11 16:24:58 +0600

    abcdefghijklmnopqrstuvwxyz

--- jan11.sql
+++ jan11.sql
@@ -15,4 +15,18 @@ where department_id in(
     select department_id
     from employees
 where last_name LIKE '%u%' );
-    
+
+select  last_name ,department_id, job_id, location_id
+from employees natural join departments
+where location_id = 1700;
+
+select  e.last_name ,e.department_id, e.job_id, d.location_id
+from employees e join departments d
+on e.department_id = d.department_id
+where location_id = 1700;  
+
+select  e.last_name ,e.department_id, e.job_id
+from employees e
+where department_id in (select department_id
+                        from departments
+                        where location_id = 1700);
