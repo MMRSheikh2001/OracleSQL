348db0a60aa299708a171f289374b5c4a9e9eb31
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-13 15:58:03 +0600

    dff

--- 13jan.sql
+++ 13jan.sql
@@ -4,6 +4,11 @@ select * from students;
 
 select * from emp1294343;
 
+commit;
+
 update emp1294343 set ename = 'Tanvir',salary =4000 where eid = 1;
 update emp1294343 set ename = 'Emon' where eid = 2;
 update emp1294343 set ename = 'Pk', address = 'Natore' where eid = 3;
+update emp1294343 set ename = 'Sajin' , address ='Jigatola' where eid = 4;
+update emp1294343 set ename = 'Sadia' , address = 'Shankar' where eid =5;
+
