0263387822ec27598c9445fa58bce15ff18c1204
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-13 17:29:54 +0600

    sffffhghff

--- 13jan.sql
+++ 13jan.sql
@@ -12,3 +12,19 @@ update emp1294343 set ename = 'Pk', address = 'Natore' where eid = 3;
 update emp1294343 set ename = 'Sajin' , address ='Jigatola' where eid = 4;
 update emp1294343 set ename = 'Sadia' , address = 'Shankar' where eid =5;
 
+create table emp1122(
+    emp_id NUMBER,
+    emp_name VARCHAR(30) not null,
+    emp_email VARCHAR(60) not null,
+    emp_salary number(10,2),
+    emp_cell VARCHAR(15),
+    
+    constraint hr_emp1122_emp_id_pk primary key(emp_id),
+    constraint hr_emp1122_emp_email_uq unique(emp_email),
+    constraint hr_emp1122_emp_cell_uq unique(emp_cell)
+);
+
+select * from emp1122;
+
+insert into emp1122 (emp_id,emp_name,emp_email,emp_salary,emp_cell)
+values (6,'Abul','abul@gmail.com',3000.00,'0165887854');
