b96902cf8b42974f95969722185e7ace60417c43
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-18 15:23:54 +0600

    gfgg

--- 18jan.sql
+++ 18jan.sql
@@ -1,5 +1,7 @@
 select * from employees;
 
+select * from emp1122;
+
 commit;
 
 rollback;
@@ -7,4 +9,10 @@ rollback;
 desc employees;
 
 --alter learn
+alter table emp1122
+add (join_date date default sysdate ,blood_grp VARCHAR(3));
+
+update emp1122
+set blood_grp ='A+'
+where emp_id = 3;
 
