cf5e031a999aeef33c731f5500d971e5ebb3248b
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-18 16:22:32 +0600

    abcdefghijklmnopqrstuvwxyz

--- 18jan.sql
+++ 18jan.sql
@@ -16,3 +16,21 @@ update emp1122
 set blood_grp ='A+'
 where emp_id = 3;
 
+alter table emp1122
+modify (join_date date default sysdate-7300);
+
+alter table emp1122
+add(hiredate date DEFAULT sysdate-456);
+
+alter table emp1122
+drop (hiredate);
+
+alter table emp1122 read only;
+
+alter table emp1122 read write;
+
+update emp1122
+set blood_grp ='B+'
+where emp_id = 5;
+
+drop table emp1122;
