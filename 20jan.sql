f117b5b783cbf311ce7ff705fe1f0f6c4eda8b68
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-20 16:31:16 +0600

    asdfghjkl

--- 20jan.sql
+++ 20jan.sql
@@ -5,3 +5,9 @@ desc emp1122;
 alter table emp1122
 add constraint hr_emp1122_emp_id_pk primary key(emp_id);
 
+alter table emp22 rename to emp1122;
+
+alter table emp1122 rename column emp_salary to salary;
+
+alter table emp1122 rename constraint hr_emp1122_emp_cell_uk
+to hr_emp1122_emp_cell_uq;
