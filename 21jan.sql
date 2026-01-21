b73fd7fe4bd2f62efd48bb665b5c51314bda2585
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-21 18:43:23 +0600

    ghfff

--- 21jan.sql
+++ 21jan.sql
@@ -1,1 +1,14 @@
 select * from employees;
+
+select department_id, avg(salary)
+from employees
+group by department_id
+having max(salary) >12000;
+
+select first_name 
+from employees
+where first_name like 'S%';
+
+select avg(salary)
+from employees
+where job_id like 'SA%';
