0bf4e5bd53e0c866ad2264333c3655bf385faa3f
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-31 18:59:46 +0600

    trrt

--- jan31.sql
+++ jan31.sql
@@ -10,6 +10,7 @@ constraint hr_emp294343_eid_pk primary key(eid)
 
 
 select * from employees;
+select * from departments;
 --2
 select department_id,avg(salary)
 from employees
@@ -22,9 +23,10 @@ from employees
 where first_name like 'S%';
 
 --4
-select avg(salary)
-from employees
-where job_id like 'SA%';
+select avg(e.salary)
+from employees e join departments d
+on e.department_id = d.department_id
+where d.department_name like 'Sales%';
 
 --5
 select first_name, last_name,hire_date 
