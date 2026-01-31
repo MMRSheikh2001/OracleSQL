09ef689c540f04a034f92ddb54718f78358e3b24
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-01-31 18:31:02 +0600

    fhggf

---
+++ jan31.sql
@@ -0,0 +1,32 @@
+--1
+CREATE table emp294343(
+eid NUMBER(10),
+ename VARCHAR2(30),
+salary number(15) default 3000,
+hire_date date default (sysdate -1300),
+address VARCHAR2(300),
+constraint hr_emp294343_eid_pk primary key(eid)
+);
+
+
+select * from employees;
+--2
+select department_id,avg(salary)
+from employees
+group by department_id
+having max(salary) >12000;
+
+--3
+select first_name, last_name
+from employees
+where first_name like 'S%';
+
+--4
+select avg(salary)
+from employees
+where job_id like 'SA%';
+
+--5
+select first_name, last_name,hire_date 
+from employees
+order by hire_date;
