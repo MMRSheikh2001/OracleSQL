46293dd3188f1d80852d94fd505e688f1040d9c0
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-04-27 17:44:25 +0600

    uiuioy

--- dbBackup/JSP.sql
+++ dbBackup/JSP.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: localhost    Database: jsp
 -- ------------------------------------------------------
--- Server version	8.0.44
+-- Server version	8.0.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -66,6 +66,32 @@ LOCK TABLES `student` WRITE;
 INSERT INTO `student` VALUES (1,'Mahbub R Rahman','mahabub.sheikh2001@gmail.com','Agargaon','01715230943'),(3,'Sabbir','sabbir@gmail.com','Kalyanpur','464564535'),(4,'Tanvir','tanvir@gmail.com','Agargaon','65746346465'),(5,'Shimul','shimul@gmail.com','Jigatola','765485556');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `student1`
+--
+
+DROP TABLE IF EXISTS `student1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student1` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `dob` varchar(45) DEFAULT NULL,
+  `fee` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student1`
+--
+
+LOCK TABLES `student1` WRITE;
+/*!40000 ALTER TABLE `student1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student1` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -76,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-26  2:27:57
+-- Dump completed on 2026-04-27 17:43:25
